faf4b4d743b7c7ef241cff8ae58b87949e1143fd
LongWen <416441008@qq.com>
2024-04-20 14:33:58 +0800

    update rpc source

--- ethereumkit/src/main/java/io/horizontalsystems/ethereumkit/models/RpcSource.kt
+++ ethereumkit/src/main/java/io/horizontalsystems/ethereumkit/models/RpcSource.kt
@@ -40,27 +40,27 @@ sealed class RpcSource {
         }
 
         fun polygonRpcHttp(): Http {
-            return Http(listOf(URI("https://polygon-rpc.com")), null)
+            return Http(listOf(URI("https://polygon-mainnet.core.chainstack.com/e9c77e1e564c041e111132211eb0df0f")), null)
         }
 
         fun optimismRpcHttp(): Http {
-            return Http(listOf(URI("https://mainnet.optimism.io")), null)
+            return Http(listOf(URI("https://optimism-mainnet.core.chainstack.com/9f3d2000dae7846908ac871ef96e18fe")), null)
         }
 
         fun arbitrumOneRpcHttp(): Http {
-            return Http(listOf(URI("https://arb1.arbitrum.io/rpc")), null)
+            return Http(listOf(URI("https://arbitrum-mainnet.core.chainstack.com/43d06a32450091e3da629e17f3d53a5e")), null)
         }
 
         fun avaxNetworkHttp(): Http {
-            return Http(listOf(URI("https://api.avax.network/ext/bc/C/rpc")), null)
+            return Http(listOf(URI("https://avalanche-mainnet.core.chainstack.com/ext/bc/C/rpc/0d78d62f3dc1baf5968e7bf78018ce02")), null)
         }
 
         fun gnosisRpcHttp(): Http {
-            return Http(listOf(URI("https://rpc.gnosischain.com")), null)
+            return Http(listOf(URI("https://nd-786-294-051.p2pify.com/4c89e746f92af4af9f76befc8dd64e59")), null)
         }
 
         fun fantomRpcHttp(): Http {
-            return Http(listOf(URI("https://rpc.fantom.network")), null)
+            return Http(listOf(URI("https://fantom-mainnet.core.chainstack.com/01d412d3dbe245ad17742e58fa017171")), null)
         }
 
     }
