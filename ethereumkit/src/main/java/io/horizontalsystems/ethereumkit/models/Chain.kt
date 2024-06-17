7dedb822fb0789f8a3c0795df48c25608d98e1dc
LongWen <416441008@qq.com>
2024-06-17 10:11:42 +0800

    update chain safe 4

--- ethereumkit/src/main/java/io/horizontalsystems/ethereumkit/models/Chain.kt
+++ ethereumkit/src/main/java/io/horizontalsystems/ethereumkit/models/Chain.kt
@@ -17,7 +17,7 @@ enum class Chain(
     Fantom(250, 60, 10_000_000, 15, false),
     EthereumGoerli(5, 1, 10_000_000, 15, true),
     EthereumRopsten(3, 1, 2_000_000, 15, true),
-//    SafeFour(6666665, 1, 2_000_000, 15, true),
+//    SafeFour(6666665, 60, 300000000, 15, false),
     SafeFour(6666666, 60, 300000000, 15, false);
 
     val isMainNet = coinType != 1
